5b0aa09b71c671cf49368a0eba39f8a2987c41aa
Tom <tom@codebolt.co.uk>
2011-10-09 20:08:17 +0100

    Adding base data provider

---
+++ toggle/DataProvider.inc.php
@@ -0,0 +1,28 @@
+<?php
+namespace toggle;
+
+/**
+ * This is data provider class for the toggle system.
+ * 
+ * @author tom
+ */
+
+class DataProvider
+{
+	public function __construct()
+	{
+		
+	}
+	
+	/**
+	 * Checks if a named code path should be run and returns a boolean value
+	 * 
+	 * @param string $name
+	 * @return bool
+	 */
+	public function check($name)
+	{
+		
+	}
+	
+}
